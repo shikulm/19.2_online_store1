9bba94bf5fa849deb53efac7e78f66784aedf33a
shikulm <shikul_m@mail.ru>
2023-08-09 18:26:34 +0400

    add error messages for verify users

--- users/views.py
+++ users/views.py
@@ -3,13 +3,17 @@ import random
 from django.conf import settings
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth import authenticate, login
 from django.core.mail import send_mail
 from django.shortcuts import render
+
 
 from django.contrib.auth.views import LoginView as BaseLoginView
 from django.contrib.auth.views import LogoutView as BaseLogoutView
 from django.urls import reverse_lazy, reverse
 from django.views.generic import CreateView, UpdateView, TemplateView, FormView
+
+from django.contrib import messages
 
 from users.forms import UserForm, UserRegisterForm, RestorePasswordForm
 from users.models import User
@@ -18,27 +22,21 @@ from users.models import User
 # Create your views here.
 
 class LoginView(BaseLoginView):
+    # Авторизация
     template_name = 'users/login.html'
 
 
 class LogoutView(LoginRequiredMixin, BaseLogoutView):
+    # Выход
     pass
 
 
 class RegisterView(CreateView):
+    # Регистрация
     model = User
     form_class = UserRegisterForm
     success_url = reverse_lazy('users:login')
     template_name = 'users/register.html'
-
-class UserUpdateView(LoginRequiredMixin, UpdateView):
-    model = User
-    success_url = reverse_lazy('users:profile')
-    form_class = UserForm
-
-    def get_object(self, queryset=None):
-        # Получаем ссылку на текущего пользователя ждя редактирования
-        return self.request.user
 
     def form_valid(self, form):
         # Отправка письма для верификации
@@ -57,6 +55,17 @@ class UserUpdateView(LoginRequiredMixin, UpdateView):
 
         return super().form_valid(form)
 
+class UserUpdateView(LoginRequiredMixin, UpdateView):
+    # Редактирование профиля
+    model = User
+    success_url = reverse_lazy('users:profile')
+    form_class = UserForm
+
+    def get_object(self, queryset=None):
+        # Получаем ссылку на текущего пользователя для редактирования
+        return self.request.user
+
+
 
 # def RestorePasswordView(request):
 #     """Восстановление пароля"""
@@ -70,6 +79,7 @@ class UserUpdateView(LoginRequiredMixin, UpdateView):
 
 
 class RestorePasswordView(FormView):
+    # Восстановление пароля
     template_name = 'users/restore_password.html'
     success_url = reverse_lazy('users:login')
     form_class = RestorePasswordForm
@@ -77,7 +87,8 @@ class RestorePasswordView(FormView):
         if form.is_valid():
             # self.object = form.save()
             email = form.data.get('UserEmail')
-            users = User.objects.filter(email=email)
+            # users = User.objects.filter(email=email)
+            users = User.objects.get(email=email)
 
             if users:
                 self.object = users[0]
@@ -100,22 +111,30 @@ class RestorePasswordView(FormView):
 
 @login_required
 def VerificationView(request, pk, key):
-    user_verify = User.objects.filter(pk=pk, key=key)
-    if user_verify:
+    # Верификация
+
+    user = User.objects.filter(pk=pk, key=key)
+    if user:
         # Успешная верифкация
         success_verify = True
-        res_verify_msg = 'Поздравляем! \n Ваша почта подтверждена!'
+        # user = user_verify[0]
+        # res_verify_msg = 'Поздравляем! \n Ваша почта подтверждена!'
 
-        user_verify[0].is_active = success_verify
-        user_verify[0].save(update_fields=['is_active'])
+        user.is_active = success_verify
+        user.save(update_fields=['is_active'])
+
+        login(request, user)
+        messages.add_message(request, messages.INFO, f'Учетная запись {user.email} активирована')
     else:
         # Ошибка верифкация
         success_verify = False
-        res_verify_msg = 'Ошибка верификации. \n Попробуйте еще раз'
+        # res_verify_msg = 'Ошибка верификации. \n Попробуйте еще раз'
+        messages.add_message(request, messages.ERROR, f'Ошибка верификации. \n Попробуйте еще раз')
     context = {
         'success_verify': success_verify,
-        'res_verify_msg': res_verify_msg
+        # 'res_verify_msg': res_verify_msg
     }
+
 
     return render(request, 'users/verification.html', context)
 
